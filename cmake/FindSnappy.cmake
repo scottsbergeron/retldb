a2ef6a262f9719b907bd9eedaea40943e26338ab
Scott Bergeron <scott.bergeron1991@gmail.com>
2025-03-09 13:21:03 -0400

    Created Custom Find Modules: Added cmake/FindLZ4.cmake - A custom module that helps CMake find the LZ4 library on different platforms Added cmake/FindSnappy.cmake - A similar module for the Snappy library Updated CI Workflow:

---
+++ cmake/FindSnappy.cmake
@@ -0,0 +1,68 @@
+# FindSnappy.cmake - Find Snappy library
+#
+# This module defines:
+#  SNAPPY_FOUND - True if Snappy is found
+#  SNAPPY_INCLUDE_DIRS - The Snappy include directories
+#  SNAPPY_LIBRARIES - The Snappy libraries
+#  SNAPPY_VERSION - The Snappy version string
+
+# Try to find Snappy using pkg-config first
+find_package(PkgConfig QUIET)
+if(PKG_CONFIG_FOUND)
+  pkg_check_modules(PC_SNAPPY QUIET snappy)
+endif()
+
+# Find the include directory
+find_path(SNAPPY_INCLUDE_DIR
+  NAMES snappy.h
+  PATHS
+    ${PC_SNAPPY_INCLUDE_DIRS}
+    /usr/include
+    /usr/local/include
+    /opt/local/include
+    $ENV{SNAPPY_DIR}/include
+  PATH_SUFFIXES snappy
+)
+
+# Find the library
+if(WIN32)
+  # For Windows, try to find the library with different possible names
+  find_library(SNAPPY_LIBRARY
+    NAMES snappy libsnappy snappy_static libsnappy_static
+    PATHS
+      ${PC_SNAPPY_LIBRARY_DIRS}
+      $ENV{SNAPPY_DIR}/lib
+      $ENV{VCPKG_INSTALLATION_ROOT}/installed/x64-windows/lib
+      $ENV{VCPKG_INSTALLATION_ROOT}/installed/x86-windows/lib
+  )
+else()
+  # For Unix-like systems
+  find_library(SNAPPY_LIBRARY
+    NAMES snappy libsnappy
+    PATHS
+      ${PC_SNAPPY_LIBRARY_DIRS}
+      /usr/lib
+      /usr/local/lib
+      /opt/local/lib
+      $ENV{SNAPPY_DIR}/lib
+  )
+endif()
+
+# Set version
+set(SNAPPY_VERSION ${PC_SNAPPY_VERSION})
+
+# Handle the QUIETLY and REQUIRED arguments and set SNAPPY_FOUND
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(SNAPPY
+  REQUIRED_VARS SNAPPY_LIBRARY SNAPPY_INCLUDE_DIR
+  VERSION_VAR SNAPPY_VERSION
+)
+
+# Set output variables
+if(SNAPPY_FOUND)
+  set(SNAPPY_LIBRARIES ${SNAPPY_LIBRARY})
+  set(SNAPPY_INCLUDE_DIRS ${SNAPPY_INCLUDE_DIR})
+endif()
+
+# Mark as advanced
+mark_as_advanced(SNAPPY_INCLUDE_DIR SNAPPY_LIBRARY) 
